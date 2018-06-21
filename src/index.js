b5dee2d8eb9650ab81dd97f04ffd2f4151bb80a7
Plmeks <plmeks94@gmail.com>
2018-06-21 12:36:22 +0300

    imports

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
-import { Provider } from "react-redux";
 
 import App from './components/App/';
+import { Provider } from "react-redux";
 import Store from "./store/";
 
 ReactDOM.render(
